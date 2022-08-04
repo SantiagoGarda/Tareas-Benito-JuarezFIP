39d0be4f92900f4a0d4efda500e9a9546b48a1f1
SantiagoGarda <santylk@gmail.com>
2022-08-03 23:00:09 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+“Qué capo, ya estoy usando github y voy hacer
+mi primer commit”.
